c741a3d1834c67b02cbdaf018f0a49b1bcb8817b
Taiwo Oduwoga <51270310+taiwo77@users.noreply.github.com>
2022-10-28 01:01:27 +0100

    Add files via upload

---
+++ gamecode.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     transform. Translate(vector3. forward)   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           float moveHorizontal = Input.GetAxis ("Horizontal");
+     float moveVertical = Input.GetAxis ("Vertical");
+     Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+     rb.AddForce (movement * speed);
+  
+    }
+}
